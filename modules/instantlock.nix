cb43a255ac0f0bade5b5025afebbb1801e16d320
Shamrock Lee <44064051+ShamrockLee@users.noreply.github.com>
2022-04-24 21:43:05 +0800

    instantlock.nix instantwm.nix: add options instantosPackages package
    instantosPackage specifies the package set to use,
    which is nessesary for both NUR-based and
    Flake-based setup.
    
    The default value is dummy, since these packages
    are not part of Nixpkgs, and there's no sane
    "default location" to use.

--- modules/instantlock.nix
+++ modules/instantlock.nix
@@ -17,13 +17,44 @@ in
           Whether to install instantlock screen locker with setuid wrapper.
         '';
       };
+      instantosPackages = mkOption {
+        # It's a legacyPackage (pkgs) kind of attribute set,
+        # which might contains non-derivation attributes.
+        type = types.attrs;
+        default = { };
+        example = literalExpression ''
+          pkgs.nur.repos.instantos # from NUR Nixpkgs overlay
+          config.nur.repos.instantos # from NUR NixOS module / HM module
+          (import /path/to/nur { inherit pkgs; }).repos.instantos # by importing NUR
+          inputs.instantnix.packages # from instantNIX flake
+          import /path/to/instantnix { inherit pkgs; } # by importing instantNIX
+        '';
+        description = ''
+          instantOS package set to use.
+
+          This is nessesary for both NUR-based and
+          Flake-based setup.
+
+          The default value is dummy, since these packages
+          are not part of Nixpkgs, and there's no sane
+          "default location" to use.
+        '';
+      };
+      package = mkOption {
+        type = types.package;
+        default = cfg.instantosPackages.instantlock;
+        defaultText = "cfg.instantosPackages.instantlock";
+        description = ''
+          instantlock package to use.
+        '';
+      };
     };
   };
 
   config = mkIf cfg.enable {
-    environment.systemPackages = [ pkgs.instantlock ];
+    environment.systemPackages = [ cfg.package ];
     security.wrappers.instantlock = {
-      source = "${pkgs.instantlock.out}/bin/instantlock";
+      source = "${cfg.package}/bin/instantlock";
       owner = "root";
       group = "root";
     };
--- modules/instantwm.nix
+++ modules/instantwm.nix
@@ -13,7 +13,40 @@ in
   ###### interface
 
   options = {
-    services.xserver.windowManager.instantwm.enable = mkEnableOption "instantwm";
+    services.xserver.windowManager.instantwm = {
+      enable = mkEnableOption "instantwm";
+
+      instantosPackages = mkOption {
+        # It's a legacyPackage (pkgs) kind of attribute set,
+        # which might contains non-derivation attributes.
+        type = types.attrs;
+        default = { };
+        example = literalExpression ''
+          pkgs.nur.repos.instantos # from NUR Nixpkgs overlay
+          config.nur.repos.instantos # from NUR NixOS module / HM module
+          (import /path/to/nur { inherit pkgs; }).repos.instantos # by importing NUR
+          inputs.instantnix.packages # from instantNIX flake
+          import /path/to/instantnix { inherit pkgs; } # by importing instantNIX
+        '';
+        description = ''
+          instantOS package set to use.
+
+          This is nessesary for both NUR-based and
+          Flake-based setup.
+
+          The default value is dummy, since these packages
+          are not part of Nixpkgs, and there's no sane
+          "default location" to use.
+        '';
+      };
+
+      package = mkOption {
+        description = "instantwm package to install";
+        type = types.package;
+        default = cfg.instantosPackages.instantwm;
+        defaultText = "cfg.instantosPackages.instantwm";
+      };
+    };
   };
 
 
@@ -26,13 +59,17 @@ in
         name = "instantwm";
         start =
           ''
-            startinstantos &
+            ${cfg.package}/bin/startinstantos &
             waitPID=$!
           '';
       };
-    programs.instantlock.enable = true;
 
-    environment.systemPackages = [ pkgs.instantwm ];
+    programs.instantlock = {
+      enable = true;
+      instantosPackages = mkDefault cfg.instantosPackages;
+    };
+
+    environment.systemPackages = [ cfg.package ];
 
   };
 
